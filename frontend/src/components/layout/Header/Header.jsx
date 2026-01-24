7f31fcf83a2dfef9480bb47cdfc028f705320972
Carlos Freitas <carlosfreitassdev@gmail.com>
2026-01-24 13:49:25 -0300

    feat(frontend): Adicionar link no header para retornar à Home

--- frontend/src/components/layout/Header/Header.jsx
+++ frontend/src/components/layout/Header/Header.jsx
@@ -1,11 +1,16 @@
+import { Link } from 'react-router-dom'
+
 import Container from '../Container/Container'
+
 import styles from './Header.module.css'
 
 function Header() {
   return (
       <header className={styles.header_container}>
         <Container>
-          <h1>GERENCIADOR DE TAREFAS</h1>
+          <Link className={styles.link} to='/' >
+            <h1>GERENCIADOR DE TAREFAS</h1>
+          </Link>
         </Container>
       </header>
   )
